ff01bd5fde8938959706387d775ca3ea6a122aa3
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-05-04 15:24:40 -0400

    add js to scroll down on page load and message add

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,27 @@ import LiveSocket from "phoenix_live_view"
 let liveSocket = new LiveSocket("/live")
 liveSocket.connect()
 
+// Select the node that will be observed for mutations
+const targetNode = document.getElementsByClassName("messages")[0]
+
+document.addEventListener("DOMContentLoaded", function() {
+  targetNode.scrollTop = targetNode.scrollHeight
+});
+// Options for the observer (which mutations to observe)
+let config = { attributes: true, childList: true, subtree: true };
+// Callback function to execute when mutations are observed
+var callback = function(mutationsList, observer) {
+  for(var mutation of mutationsList) {
+    if (mutation.type == 'childList') {
+      targetNode.scrollTop = targetNode.scrollHeight
+    }
+  }
+};
+// Create an observer instance linked to the callback function
+var observer = new MutationObserver(callback);
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
+
 // Import local files
 //
 // Local files can be imported directly using relative paths, for example:
