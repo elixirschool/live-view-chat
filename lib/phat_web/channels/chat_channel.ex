08ff142a11d9b633a989a152dd1497d3775db18a
sophiedebenedetto <sophie.debenedetto@gmail.com>
2019-05-19 14:47:58 -0400

    use intercept

--- lib/phat_web/channels/chat_channel.ex
+++ lib/phat_web/channels/chat_channel.ex
@@ -1,12 +1,11 @@
 defmodule PhatWeb.ChatChannel do
   use Phoenix.Channel
 
-  defp topic(chat_id), do: "event_bus:#{chat_id}"
-
-  def join("event_bus:" <> chat_id, _message, socket) do
-    PhatWeb.Endpoint.subscribe(topic(chat_id))
+  def join("event_bus:" <> _chat_id, _message, socket) do
     {:ok, socket}
   end
+
+  intercept ["new_message"]
 
   def handle_out("new_message", %{current_user_id: current_user_id}, socket) do
     if  current_user_id == socket.assigns.current_user_id do
